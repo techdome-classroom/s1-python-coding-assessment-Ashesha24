21966edd09542e8ee99f5af94d31d1a1fe38ae50
Ashesha Boga <ashesha24@gmail.com>
2024-11-11 14:27:04 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,22 +1,30 @@
 def decode_message(s: str, p: str) -> bool:
     s_index = 0
     p_index = 0
-    
-    while p_index < len(p):
-        if p[p_index] == '*':
-            p_index += 1
-            if p_index == len(p):
-                return True
-            while s_index < len(s) and (s[s_index] != p[p_index] or p[p_index] == '?'):
-                s_index += 1
-            if s_index == len(s):
-                return False
-        elif p[p_index] == '?':
+    star_index = -1
+    match_index = -1
+
+    while s_index < len(s):
+        # If current characters match or pattern has a '?'
+        if p_index < len(p) and (p[p_index] == s[s_index] or p[p_index] == '?'):
             s_index += 1
             p_index += 1
+        # If pattern has a '*' (match any sequence)
+        elif p_index < len(p) and p[p_index] == '*':
+            star_index = p_index
+            match_index = s_index
+            p_index += 1
+        # If there was a previous '*' and current character doesn't match
+        elif star_index != -1:
+            p_index = star_index + 1
+            match_index += 1
+            s_index = match_index
         else:
-            if s_index >= len(s) or s[s_index] != p[p_index]:
-                return False
-            s_index += 1
-            p_index += 1
-return False
+            return False
+
+    # Check if remaining pattern is all '*'
+    while p_index < len(p) and p[p_index] == '*':
+        p_index += 1
+
+    return p_index == len(p)
+
