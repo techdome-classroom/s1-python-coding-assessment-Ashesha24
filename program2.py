2a825a0d58347fd956bed29b199241595172602d
Ashesha Boga <ashesha24@gmail.com>
2024-11-11 14:24:56 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,17 +1,4 @@
-def decode_message( s: str, p: str) -> bool:
 
-
-    s_index = 0
-    p_index = 0
-    
-    while p_index < len(p):
-        if p[p_index] == '*':
-            p_index += 1
-            if p_index == len(p):
-                return True
-            while s_index < len(s) and (s[s_index] != p[p_index] or p[p_index] == '?'):
-                s_index += 1
-            if s_index == len(s):
                 return False
         elif p[p_index] == '?':
             s_index += 1
